d2ee48570832f3d4bee3875aed969097aab94d07
lizastrappini <lizastrappini99@gmail.com>
2026-04-22 22:14:33 -0300

    cambio

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
-    f"@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DATABASE')}"
+    f"@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT', '3306')}/{os.getenv('MYSQL_DATABASE')}"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
